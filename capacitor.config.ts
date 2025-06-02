a78d44cc7a15149637a7746510a9f96c0833f202
aldrian03 <alddriancayomoc@gmail.com>
2025-06-02 10:08:43 +0800

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.system',
+  appName: 'io-system',
+  webDir: 'dist'
+};
+
+export default config;
